af87dec86e3e1d0a410a0cc86312f1969d77e8a7
LourYde <yehor.stohnii@nure.ua>
2026-03-21 21:43:59 +0200

    Єгор Стогній update

---
+++ ProfileEditing.js
@@ -0,0 +1,8 @@
+const app = document.createElement('div');
+app.style.fontFamily = 'sans-serif';
+app.style.maxWidth = '400px';
+app.style.margin = '50px auto';
+app.style.padding = '20px';
+app.style.border = '1px solid #ddd';
+app.style.borderRadius = '8px';
+document.body.appendChild(app);
