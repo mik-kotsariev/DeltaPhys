aafdef8c6df3cd6219e7c33d96fbd62e046779a1
mik-kotsariev <mykola.kotsariev@nure.ua>
2026-06-03 04:05:13 +0300

    Самостійна робота

--- Lr1 Hladkykh/GeometryHelper.cs
+++
@@ -1,7 +0,0 @@
-// Функція для обчислення площі трикутника за трьома сторонами (формула Герона)
-
-public static double GetTriangleArea(double a, double b, double c)
-{
-    double p = (a + b + c) / 2;
-    return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-}
--- Lr1 Hladkykh/Program.cs
+++
@@ -1,4 +0,0 @@
-Console.WriteLine("Введіть радіус:");
-double r = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"Площа: {Math.PI * r * r}");
- 
